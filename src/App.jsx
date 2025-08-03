66570b920655ef58f19597438607287a4a79f0b9
jeongchaehwan <wjdcoghks1428@naver.com>
2025-08-03 21:24:43 +0900

    회원가입 홈 개발 완료 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import { useState } from 'react'
-import { useAtom } from 'jotai'
-import { countAtom } from './atoms/counter'
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import SignupForm from './components/SignupForm';
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const [count, setCount] = useAtom(countAtom)
-
   return (
-    <>
-      <div className='text-3xl font-bold text-blue-500'>
-        {count}
-      </div>
-      <button onClick={() => {setCount(count+1)}}>+1</button>
-    </>
-  )
+    <div className="min-h-screen flex items-center justify-center bg-gray-50">
+      <SignupForm />
+    </div>
+  );
 }
 
-export default App
+export default App;
