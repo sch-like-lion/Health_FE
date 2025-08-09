bf6e8feeb6e0c6f0e1a64333f5fbec1011848090
Yuno Kim <unokim1547@gmail.com>
2025-08-09 19:52:12 +0900

    loginAPI connect

--- src/components/LoginPage/LoginForm/LoginForm.jsx
+++ src/components/LoginPage/LoginForm/LoginForm.jsx
@@ -6,19 +6,53 @@ import Button from "@/components/common/Button/Button";
 import Link from "next/link";
 
 export default function LoginForm() {
-  const [loginData, setLoginData] = useState({
-    userid: "",
-    password: "",
-  });
+  const [id, setId] = useState("")
+  const [pw, setPw] = useState("")
+  const [errMessage, setErrMessage] = useState("")
+
+  const loginRequest = async () => {
+    try {
+      const response = await fetch("http://fitlog.iubns.net:8080/api/users/login", {
+        method : "POST",
+        headers : {
+          'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+          customId : id,
+          password : pw
+        })
+      })
+
+      // unauthorized등의 응답 에러
+      if(!response.ok) {
+        const errorData = await response.json().catch(() => ({}))
+        throw new Error(errorData.message || '알수없는 서버 오류')
+      }
+
+      const data = await response.json()
+      const token = data.token
+      console.log('로그인성공', data);
+      console.log('token', token);
+      // 로그인 성공시 리다이렉팅 해주는게 좋을듯
+    } catch(err) {
+      setErrMessage(err.message)
+      console.error('로그인실패', err.message)
+    }
+  }
+
   const loginSubmitHandler = e => {
     e.preventDefault();
+    console.log(id, pw);
+    loginRequest() 
   };
+
   return (
     <>
-      <form className="flex flex-col gap-2" onSubmit={loginSubmitHandler}>
-        <Input type="text" placeholder="아이디" />
-        <Input type="password" placeholder="비밀번호" />
+      <form className="relative flex flex-col gap-2" onSubmit={loginSubmitHandler}>
+        <Input value={id} onChange={e => setId(e.target.value)} type="text" placeholder="아이디" />
+        <Input value={pw} onChange={e => setPw(e.target.value)} type="password" placeholder="비밀번호" />
         <Button>로그인</Button>
+        <span className="absolute bottom-[-25px] text-red-600 text-center w-full">{errMessage}</span>
       </form>
       <div className="flex justify-center gap-2 mt-4">
         <Link href={"/login/id-lost"} className="inline-block p-2 text-sm text-gray-500">아이디 찾기</Link>
